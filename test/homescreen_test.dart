38bf902c6314c15dc0d9bc6eb0fa29acb89fb5e2
maheini35 <maheini35@me.com>
2022-01-25 23:10:43 +0100

    first tests for Home Screen and created Mocks in advance

--- test/homescreen_test.dart
+++ test/homescreen_test.dart
@@ -1,10 +1,13 @@
+import 'package:carbpro/handler/databasehandler.dart';
 import 'package:carbpro/newmain.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
+import 'package:mockito/annotations.dart';
+import 'homescreen_test.mocks.dart';
 
 // flutter pub run build_runner build --delete-conflicting-outputs
 
-// @GenerateMocks([CarbPro])
+@GenerateMocks([DatabaseHandler])
 void main(){
   group('App startup', () {
     testWidgets('Open the App and check for basic design elements', (WidgetTester tester) async{
