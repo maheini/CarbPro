c53d3f49e939d1124e053539ff6b483385b9edc1
maheini35 <maheini35@me.com>
2022-01-27 23:29:16 +0100

    added test for removing items

--- test/homescreen_test.dart
+++ test/homescreen_test.dart
@@ -1,6 +1,8 @@
 import 'package:carbpro/datamodels/item.dart';
+import 'package:carbpro/datamodels/itemchild.dart';
 import 'package:carbpro/handler/databasehandler.dart';
 import 'package:carbpro/handler/storagehandler.dart';
+import 'package:carbpro/homescreen.dart';
 import 'package:carbpro/locator/locator.dart';
 import 'package:carbpro/main.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +10,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'homescreen_test.mocks.dart';
 
@@ -183,6 +186,101 @@ void main(){
       expect(find.text('Item1'), findsOneWidget);
       expect(find.text('Item2'), findsOneWidget);
 
+      locator.resetScope(dispose: true);
+    });
+
+  });
+
+  group('Test Item List', () {
+    testWidgets('DetailScreen should be loaded after a click onto a Item', (WidgetTester tester) async{
+      MockDatabaseHandler databaseHandler = MockDatabaseHandler();
+      locator.registerSingleton<StorageHandler>(StorageHandler(FileAccessWrapper()));
+      locator.registerSingleton<DatabaseHandler>(databaseHandler);
+      when(databaseHandler.getItems()).thenAnswer((_) async => Future.value([Item(1, 'Item1')]));
+      RouteSettings? routeSettings;
+
+      await tester.pumpWidget(MaterialApp(
+        onGenerateRoute: (settings) {
+          routeSettings = settings;
+          if(settings.name == '/' || settings.name == '/details'){
+            return MaterialPageRoute(builder: (_) => const HomeScreen());
+          }
+          return null;
+        }
+      ));
+      await tester.pump();
+
+      // tap item1
+      await tester.tap(find.text('Item1'));
+      await tester.pump();
+
+      //check if route '/details' was called with argument int = 1
+      expect(routeSettings, isNotNull);
+      expect(routeSettings!.arguments as int, 1);
+      locator.resetScope(dispose: true);
+    });
+
+    testWidgets('A Popup should appear after lonPressing on an Item'
+        '-After tapping the cancel button, everything should be canceled'
+        '-After pressing confirm, the item should disappear', (WidgetTester tester) async {
+      // prepare dependencies
+      MockDatabaseHandler databaseHandler = MockDatabaseHandler();
+      MockStorageHandler storageHandler = MockStorageHandler();
+      locator.registerSingleton<StorageHandler>(storageHandler);
+      locator.registerSingleton<DatabaseHandler>(databaseHandler);
+      when(databaseHandler.getItems()).thenAnswer((_) async => Future.value([Item(1, 'Item1')]));
+
+      // load widget
+      await tester.pumpWidget(const CarbPro());
+      await tester.pump();
+
+      // longPress item1
+      await tester.longPress(find.text('Item1'));
+      await tester.pump();
+
+      // expect a Alertdialog containing a text with a 'ABBRECHEN' or 'ENTFERNEN' option
+      expect(find.byType(AlertDialog), findsOneWidget);
+      expect(find.text('ENTFERNEN'), findsOneWidget);
+      expect(find.text('ABBRECHEN'), findsOneWidget);
+
+      // press 'ABBRECHEN'
+      await tester.tap(find.text('ABBRECHEN'));
+      await tester.pump();
+
+      // Expect the initial list, containing all items as before and no alert dialog
+      expect(find.text('Item1'), findsOneWidget);
+      expect(find.byType(AlertDialog), findsNothing);
+
+      // Prepare mocks for removing the Item
+      when(databaseHandler.getChildren(1))
+          .thenAnswer((realInvocation) => Future.value([ItemChild(11,1,'', 'path')]));
+      when(databaseHandler.getItems())
+          .thenAnswer((realInvocation) => Future.value([]));
+      when(storageHandler.getPermission(Permission.storage, any))
+          .thenAnswer((realInvocation) => Future.value(true));
+      when(storageHandler.deleteFile('path'))
+          .thenAnswer((realInvocation) => Future.value());
+
+      // longPress item1 again to remove it this time
+      await tester.longPress(find.text('Item1'));
+      await tester.pump();
+
+      // press 'ENTFERNEN'
+      await tester.tap(find.text('ENTFERNEN'));
+
+      // check if every necessary function get called
+      verify(storageHandler.getPermission(Permission.storage, any)).called(1);
+      verify(databaseHandler.getChildren(1)).called(1);
+      verify(storageHandler.deleteFile('path')).called(1);
+
+      // load to check if item isn't visible anymore
+      await tester.pump();
+
+      // expect no longer any Item1 inside the list and no alertdialog
+      expect(find.text('Item1'), findsNothing);
+      expect(find.byType(AlertDialog), findsNothing);
+
+      // reset locator
       locator.resetScope(dispose: true);
     });
 
