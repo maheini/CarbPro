38e6ac77c551c3efeeeab23fdd22597dd263ff92
maheini35 <maheini35@me.com>
2022-01-28 21:32:59 +0100

    first version of addbutton tests

--- test/detailscreen_test.dart
+++ test/detailscreen_test.dart
@@ -1,5 +1,4 @@
 import 'package:carbpro/datamodels/item.dart';
-import 'package:carbpro/datamodels/itemchild.dart';
 import 'package:carbpro/detailscreen.dart';
 import 'package:carbpro/locator/locator.dart';
 import 'package:flutter/material.dart';
@@ -8,11 +7,12 @@ import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:carbpro/handler/databasehandler.dart';
 import 'package:carbpro/handler/storagehandler.dart';
+import 'package:image_picker/image_picker.dart';
 
 import 'detailscreen_test.mocks.dart';
 
 
-@GenerateMocks([DatabaseHandler, StorageHandler])
+@GenerateMocks([DatabaseHandler, StorageHandler, ImagePicker])
 void main() {
   group('Test Detailscreen AppBar', () {
     MockDatabaseHandler databaseHandler = MockDatabaseHandler();
@@ -83,4 +83,96 @@ void main() {
       locator.resetScope(dispose: true);
     });
   });
+
+  group('Test add Button behaviour', () {
+    MockDatabaseHandler databaseHandler = MockDatabaseHandler();
+    MockStorageHandler storageHandler = MockStorageHandler();
+    setUp(() {
+      locator.registerSingleton<DatabaseHandler>(databaseHandler);
+      locator.registerSingleton<StorageHandler>(storageHandler);
+    });
+
+    testWidgets('Check if Addbutton is aviable -> after clicking on it, '
+        'a popup should appear with with a field of name and image', (WidgetTester tester) async {
+      when(databaseHandler.getItem(1)).thenAnswer((_) async => Future.value(Item(1, 'ItemName')));
+      when(databaseHandler.getChildren(1)).thenAnswer((_) async => Future.value([]));
+      
+      await tester.pumpWidget(const MaterialApp(home: DetailScreen(id: 1)));
+      await tester.pump();
+      
+      // tap add-item button
+      await tester.tap(find.byIcon(Icons.add_a_photo_outlined));
+      await tester.pump();
+      
+      // expect a popup with textfield and add-image icon
+      expect(find.byType(TextField), findsOneWidget);
+      expect(find.byIcon(Icons.wallpaper), findsOneWidget);
+      expect(find.text('SPEICHERN'), findsOneWidget);
+      expect(find.text('ABBRECHEN'), findsOneWidget);
+      
+      // try pressing save -> the popup should be closed, nothing should happen
+      verifyNever(databaseHandler.addItemChild(any));
+      await tester.tap(find.text('SPEICHERN'));
+      await tester.pump();
+
+      // expect home screen
+      expect(find.byType(TextField), findsNothing);
+      expect(find.byIcon(Icons.wallpaper), findsNothing);
+      expect(find.text('SPEICHERN'), findsNothing);
+      expect(find.text('ABBRECHEN'), findsNothing);
+      
+      // again add item, press ´ABBRECHEN´ this time
+      await tester.tap(find.byIcon(Icons.add_a_photo_outlined));
+      await tester.pump();
+      verifyNever(databaseHandler.addItemChild(any));
+      await tester.tap(find.text('ABBRECHEN'));
+      await tester.pump();
+    });
+
+    testWidgets('click addbutton and afterwards try to add an image & text & store them', 
+            (WidgetTester tester) async {
+      MockImagePicker imagePicker = MockImagePicker();
+      when(imagePicker.pickImage(source: ImageSource.camera))
+          .thenAnswer((realInvocation) => Future.value(null));  // todo: pass fake image
+      when(databaseHandler.getItem(1)).thenAnswer((_) async => Future.value(Item(1, 'ItemName')));
+      when(databaseHandler.getChildren(1)).thenAnswer((_) async => Future.value([]));
+
+      await tester.pumpWidget(const MaterialApp(home: DetailScreen(id: 1)));
+      await tester.pump();
+
+      // tap add-item button
+      await tester.tap(find.byIcon(Icons.add_a_photo_outlined));
+      await tester.pump();
+      
+      // Pick image and enter name
+      locator.registerLazySingleton<ImagePicker>(() => imagePicker);
+      await tester.tap(find.byIcon(Icons.wallpaper));
+      await tester.enterText(find.byType(TextField), 'ItemChild');
+      await tester.pump();
+
+      // save everything
+      when(databaseHandler.addItemChild(any))
+          .thenAnswer((realInvocation) => Future.value(2));
+      when(storageHandler.copyFile(any, any))
+          .thenAnswer((_) => Future.value()); // todo: pass fake file
+      verify(databaseHandler.addItem(any)).called(1);
+      verify(storageHandler.copyFile(any, any)).called(1);
+      await tester.tap(find.text('SPEICHERN'));
+      await tester.pumpAndSettle();
+
+      // expect home with one widget to show up
+      expect(find.byType(TextField), findsNothing);
+      expect(find.byIcon(Icons.wallpaper), findsNothing);
+      expect(find.text('SPEICHERN'), findsNothing);
+      expect(find.text('ABBRECHEN'), findsNothing);
+      expect(find.text('ItemChild'), findsOneWidget);
+    });
+
+
+    tearDown(() {
+      // reset locator
+      locator.resetScope(dispose: true);
+    });
+  });
+
 }
