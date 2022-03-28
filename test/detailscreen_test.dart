e0cab4f5fd1a6ddcc4a15d409ca7002b43895522
maheini35 <maheini35@me.com>
2022-03-28 11:15:00 +0200

    translation support for detailscreen_test

--- test/detailscreen_test.dart
+++ test/detailscreen_test.dart
@@ -11,6 +11,8 @@ import 'package:image_picker/image_picker.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:carbpro/generated/l10n.dart';
 
 import 'detailscreen_test.mocks.dart';
 
@@ -52,7 +54,18 @@ void main() {
           .thenAnswer((_) async => Future.value([]));
 
       // start app
-      await tester.pumpWidget(const MaterialApp(home: DetailScreen(id: 1)));
+      await tester.pumpWidget(
+        MaterialApp(
+          localizationsDelegates: const [
+            S.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: S.delegate.supportedLocales,
+          home: const DetailScreen(id: 1),
+        ),
+      );
       await tester.pump();
 
       // press edit button
@@ -62,11 +75,11 @@ void main() {
       // Check if Popup is visible
       expect(find.text('ItemName'), findsNWidgets(2));
       expect(find.byType(TextField), findsOneWidget);
-      expect(find.text('ABBRECHEN'), findsOneWidget);
-      expect(find.text('SPEICHERN'), findsOneWidget);
+      expect(find.text(S.current.cancel.toUpperCase()), findsOneWidget);
+      expect(find.text(S.current.save.toUpperCase()), findsOneWidget);
 
       // press cancel and check if popup disappears
-      await tester.tap(find.text('ABBRECHEN'));
+      await tester.tap(find.text(S.current.cancel.toUpperCase()));
       await tester.pump();
       expect(find.byType(TextField), findsNothing);
       expect(find.text('ItemName'), findsOneWidget);
@@ -77,7 +90,7 @@ void main() {
 
       // clear text and try to save (should not save)
       await tester.enterText(find.byType(TextField), '');
-      await tester.tap(find.text('SPEICHERN'));
+      await tester.tap(find.text(S.current.save.toUpperCase()));
       verifyNever(databaseHandler.changeItemName(any, any));
 
       // change name
@@ -86,7 +99,7 @@ void main() {
       when(databaseHandler.getItem(1))
           .thenAnswer((realInvocation) => Future.value(Item(1, 'NewItemName')));
       await tester.enterText(find.byType(TextField), 'NewItemName');
-      await tester.tap(find.text('SPEICHERN'));
+      await tester.tap(find.text(S.current.save.toUpperCase()));
       verify(databaseHandler.changeItemName(1, 'NewItemName')).called(1);
       await tester.pump();
 
@@ -120,7 +133,18 @@ void main() {
       when(databaseHandler.getChildren(1))
           .thenAnswer((_) async => Future.value([]));
 
-      await tester.pumpWidget(const MaterialApp(home: DetailScreen(id: 1)));
+      await tester.pumpWidget(
+        MaterialApp(
+          localizationsDelegates: const [
+            S.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: S.delegate.supportedLocales,
+          home: const DetailScreen(id: 1),
+        ),
+      );
       await tester.pump();
 
       // tap add-item button
@@ -130,25 +154,25 @@ void main() {
       // expect a popup with textfield and add-image icon
       expect(find.byType(TextField), findsOneWidget);
       expect(find.byIcon(Icons.add_photo_alternate_outlined), findsOneWidget);
-      expect(find.text('SPEICHERN'), findsOneWidget);
-      expect(find.text('ABBRECHEN'), findsOneWidget);
+      expect(find.text(S.current.save.toUpperCase()), findsOneWidget);
+      expect(find.text(S.current.cancel.toUpperCase()), findsOneWidget);
 
       // try pressing save -> the popup should be closed, nothing should happen
       verifyNever(databaseHandler.addItemChild(any));
-      await tester.tap(find.text('SPEICHERN'));
+      await tester.tap(find.text(S.current.save.toUpperCase()));
       await tester.pump();
 
       // expect home screen
       expect(find.byType(TextField), findsNothing);
       expect(find.byIcon(Icons.wallpaper), findsNothing);
-      expect(find.text('SPEICHERN'), findsNothing);
-      expect(find.text('ABBRECHEN'), findsNothing);
+      expect(find.text(S.current.save.toUpperCase()), findsNothing);
+      expect(find.text(S.current.cancel.toUpperCase()), findsNothing);
 
       // again add item, press ´ABBRECHEN´ this time
       await tester.tap(find.byIcon(Icons.add_a_photo_outlined));
       await tester.pump();
       verifyNever(databaseHandler.addItemChild(any));
-      await tester.tap(find.text('ABBRECHEN'));
+      await tester.tap(find.text(S.current.cancel.toUpperCase()));
       await tester.pump();
     });
 
@@ -175,7 +199,18 @@ void main() {
       when(storageHandler.exists(any))
           .thenAnswer((realInvocation) => Future.value(true));
 
-      await tester.pumpWidget(const MaterialApp(home: DetailScreen(id: 1)));
+      await tester.pumpWidget(
+        MaterialApp(
+          localizationsDelegates: const [
+            S.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: S.delegate.supportedLocales,
+          home: const DetailScreen(id: 1),
+        ),
+      );
       await tester.pump();
 
       // tap add-item button
@@ -200,8 +235,8 @@ void main() {
           (_) => Future.value(File('assets/storagehandler_test_image.jpg')));
       when(databaseHandler.getChildren(1)).thenAnswer(
           (_) async => Future.value([ItemChild(1, 1, 'ItemChild', '')]));
-      expect(find.text('SPEICHERN'), findsOneWidget);
-      await tester.tap(find.text('SPEICHERN'));
+      expect(find.text(S.current.save.toUpperCase()), findsOneWidget);
+      await tester.tap(find.text(S.current.save.toUpperCase()));
       await tester.pump();
       verify(storageHandler.copyFile(any, any)).called(1);
       verify(databaseHandler.addItemChild(any)).called(1);
@@ -212,8 +247,8 @@ void main() {
       // expect home with one widget to show up
       expect(find.byType(TextField), findsNothing);
       expect(find.byIcon(Icons.add_photo_alternate_outlined), findsNothing);
-      expect(find.text('SPEICHERN'), findsNothing);
-      expect(find.text('ABBRECHEN'), findsNothing);
+      expect(find.text(S.current.save.toUpperCase()), findsNothing);
+      expect(find.text(S.current.cancel.toUpperCase()), findsNothing);
       expect(find.text('ItemChild'), findsOneWidget);
     });
 
