314d734f3eef1d3c013765503ec39dbb6504b7f4
maheini35 <maheini35@me.com>
2022-04-06 13:26:06 +0200

    new Tests

--- test/ui/widgets/itemlist_test.dart
+++ test/ui/widgets/itemlist_test.dart
@@ -1,10 +1,12 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:carbpro/bloc/list_cubit/list_cubit.dart';
+import 'package:carbpro/datamodels/item.dart';
+import 'package:carbpro/detailscreen.dart';
 import 'package:carbpro/ui/widgets/itemlist.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MockListCubit extends MockCubit<ListState> implements ListCubit {}
 
@@ -28,14 +30,40 @@ void main() {
         verifyNever(() => listCubit.loadItems());
 
         await tester.pumpWidget(
-          BlocProvider(
-            create: (_) => listCubit,
-            child: const ItemList(),
+          MaterialApp(
+            home: BlocProvider(
+              create: (_) => listCubit,
+              child: const ItemList(),
+            ),
           ),
         );
 
         expect(find.byType(CircularProgressIndicator), findsOneWidget);
       });
+
+      testWidgets('ItemList should display two Items after its loaded',
+          (WidgetTester tester) async {
+        when(() => listCubit.state).thenReturn(
+          ListLoaded(
+            [Item(0, 'item1'), Item(1, 'item2')],
+            const [],
+          ),
+        );
+
+        await tester.pumpWidget(
+          MaterialApp(
+            home: BlocProvider(
+              create: (_) => listCubit,
+              child: const ItemList(),
+            ),
+          ),
+        );
+
+        expect(find.text('item1'), findsOneWidget);
+        expect(find.text('item2'), findsOneWidget);
+      });
+    },
+  );
     },
   );
 }
