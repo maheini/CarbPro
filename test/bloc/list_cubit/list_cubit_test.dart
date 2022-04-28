afde0d2d8842f507d54d249efdce26db71ec48da
maheini35 <maheini35@me.com>
2022-04-29 00:12:30 +0200

    export function test

--- test/bloc/list_cubit/list_cubit_test.dart
+++ test/bloc/list_cubit/list_cubit_test.dart
@@ -474,4 +474,31 @@ void main() {
       ],
     );
   });
+
+  group('Test the export function', () {
+    List<Item> items = [Item(0, 'item1'), Item(1, 'item2')];
+    List<ItemChild> child = [ItemChild(0, 1, '', 'imagepath')];
+    late MockDatabaseHandler databaseHandler;
+    late MockStorageHandler storageHandler;
+
+    setUp(() {
+      databaseHandler = MockDatabaseHandler();
+      storageHandler = MockStorageHandler();
+
+      when(() => databaseHandler.loadDatabase()).thenAnswer((_) async => true);
+      when(() => databaseHandler.getItems()).thenAnswer((_) async => items);
+      when(() => databaseHandler.getChildren(1)).thenAnswer((_) async => child);
+    });
+
+    blocTest(
+      'If the state is not ListSelection, false should be returned '
+      'and nothing should happen',
+      build: () => ListCubit(databaseHandler, storageHandler),
+      act: (ListCubit cubit) async {
+        await cubit.loadItems();
+        expect(await cubit.export(), false);
+      },
+      verify: (_) =>
+          verifyNever(() => storageHandler.getPermission(any(), any())),
+    );
 }
