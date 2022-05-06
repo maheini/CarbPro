1e26f9554cc69aa0d1108be3d04bc243c7fda16d
maheini35 <maheini35@me.com>
2022-05-07 01:43:21 +0200

    improved about-screen

--- lib/ui/aboutscreen.dart
+++ lib/ui/aboutscreen.dart
@@ -1,9 +1,15 @@
 import 'package:carbpro/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-class AboutScreen extends StatelessWidget {
+class AboutScreen extends StatefulWidget {
   const AboutScreen({Key? key}) : super(key: key);
 
+  @override
+  State<AboutScreen> createState() => _AboutScreenState();
+}
+
+class _AboutScreenState extends State<AboutScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +17,32 @@ class AboutScreen extends StatelessWidget {
         title: Text(S.current.about),
         centerTitle: true,
       ),
-      body: const Text('about carbpro'),
+
+  Widget _buildAppInfo() {
+    return SizedBox(
+      height: 20,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('${S.current.version}: $_version'),
+          const SizedBox(width: 10),
+          Text('${S.current.build}: $_buildNumber'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAppDescription() {
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 700),
+      padding: const EdgeInsets.all(20),
+      child: Text(
+        S.current.app_description,
+        style: const TextStyle(
+          fontSize: 17,
+        ),
+        textAlign: TextAlign.justify,
+      ),
     );
   }
 }
