d50a2767b800cc552fbb99b62fb82a394a9e3df4
maheini35 <maheini35@me.com>
2022-05-27 11:39:08 +0200

    test emptylistplaceholder dark theme behaviour

--- test/ui/widgets/emptylistplaceholder_test.dart
+++ test/ui/widgets/emptylistplaceholder_test.dart
@@ -21,5 +21,27 @@ void main() {
       expect(find.byType(Image), findsOneWidget);
     });
 
+    testWidgets('If theme is dark, arrow_white.png should be displayed',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          theme: ThemeData(),
+          darkTheme: ThemeData.dark(),
+          themeMode: ThemeMode.dark,
+          home: const EmptyListPlaceholder(
+            text: 'message',
+          ),
+        ),
+      );
+
+      final Image image = find.byType(Image).evaluate().single.widget as Image;
+
+      String source = '';
+      if (image.image is AssetImage) {
+        source = (image.image as AssetImage).assetName;
+      }
+      expect(source, 'assets/arrow_white.png');
+    });
+
   });
 }
