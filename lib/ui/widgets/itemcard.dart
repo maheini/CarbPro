7113f236109f1a97ac98cbf04e1f9d379f46349e
maheini35 <maheini35@me.com>
2022-06-23 23:39:18 +0200

    new ItemCard Design & carb amounts

--- lib/ui/widgets/itemcard.dart
+++ lib/ui/widgets/itemcard.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class ItemCard extends StatelessWidget {
@@ -25,39 +26,82 @@ class ItemCard extends StatelessWidget {
         color: Colors.black.withOpacity(0.2),
       ),
       margin: const EdgeInsets.all(7),
-      child: Container(
-        //CONTENT
-        padding: const EdgeInsets.all(5),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Flexible(
-              flex: 17,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(3),
-                child: AspectRatio(
-                  aspectRatio: 1,
-                  child: image != null
-                      ? Image.file(
-                          image!,
-                          fit: BoxFit.cover,
-                        )
-                      : const Icon(Icons.wallpaper),
-                ),
-              ),
-            ),
-            const Spacer(
-              flex: 1,
-            ),
-            Flexible(
-              flex: 2,
-              child: Text(
+      child: InkWell(
+        onTap: () => onTap,
+        onLongPress: () => onLongPress,
+        child: Container(
+          //CONTENT
+          padding: const EdgeInsets.all(5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 5),
+              Text(
                 title,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
                 overflow: TextOverflow.ellipsis,
               ),
-            )
-          ],
+              const SizedBox(height: 5),
+              Stack(
+                alignment: AlignmentDirectional.bottomCenter,
+                children: [
+                  Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(3),
+                        child: AspectRatio(
+                          aspectRatio: 1,
+                          child: image != null
+                              ? Image.file(
+                                  image!,
+                                  fit: BoxFit.cover,
+                                )
+                              : const Icon(Icons.wallpaper),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(4),
+                        color: Colors.blue[800],
+                        child: Expanded(
+                          child: DottedBorder(
+                            borderType: BorderType.Rect,
+                            strokeWidth: 2,
+                            color: Colors.white.withOpacity(0.7),
+                            dashPattern: const [6, 4],
+                            child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 5),
+                                child: Text(
+                                  value + ' g KH',
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
