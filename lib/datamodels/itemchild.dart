9d8d90aff450c0c21d0d22582b04c2a15d058744
maheini35 <maheini35@me.com>
2022-06-23 23:55:54 +0200

    extended itemchild with a carb value

--- lib/datamodels/itemchild.dart
+++ lib/datamodels/itemchild.dart
@@ -1,7 +1,9 @@
 class ItemChild {
-  ItemChild(this.id, this.parentID, this.description, this.imagepath);
+  ItemChild(
+      this.id, this.parentID, this.description, this.value, this.imagepath);
   final int id;
   final int parentID;
+  double value;
   String description;
   String imagepath;
 }
