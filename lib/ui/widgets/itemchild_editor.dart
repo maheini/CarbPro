9103bcdff3c278ee62098130627d071be3bb5e15
maheini35 <maheini35@me.com>
2022-09-15 23:16:18 +0200

    itemChild editor

--- lib/ui/widgets/itemchild_editor.dart
+++ lib/ui/widgets/itemchild_editor.dart
@@ -55,7 +55,103 @@ class _ItemChildEditorState extends State<ItemChildEditor> {
 
   @override
   Widget build(BuildContext context) {
+    return AlertDialog(
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _nameController,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+              decoration: InputDecoration(
+                hintText: S.of(context).description,
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                _pickImage().then((file) {
+                  if (file != null) {
+                    setState(() => image = file);
+                  }
+                });
+              },
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(4),
+                  child: image != null
+                      ? Image.file(
+                          image!,
+                          fit: BoxFit.cover,
+                        )
+                      : Center(
+                          child: CircleAvatar(
+                            radius: 45,
+                            backgroundColor: Colors.black.withOpacity(0.1),
+                            child: const Icon(
+                              Icons.add_photo_alternate_outlined,
+                              size: 50,
+                            ),
+                          ),
+                        ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                controller: _valueController,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  hintText: S.of(context).value,
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.allow(RegExp(r'^[\d./-]+$')),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+          child: Text(S.of(context).cancel.toUpperCase()),
+          onPressed: () => widget.onCancel?.call(),
+        ),
+        TextButton(
+          child: Text(S.of(context).save.toUpperCase()),
+          onPressed: () {
+            ItemChild oldItem = widget.itemChild;
 
+            bool imageHasChanged = false;
+
+            final String name = _nameController.text;
+            final double value = double.tryParse(_valueController.text) ?? 0.0;
+            String imagepath = oldItem.imagepath;
+            // Check if image is not null and is different to widget.itemChild.image
+            if (image != null &&
+                oldItem.imagepath != path.basename(image!.path)) {
+              imageHasChanged = true;
+              imagepath = image!.path;
+            }
+
+            ItemChild newItem = ItemChild(
+              oldItem.id,
+              oldItem.parentID,
+              name,
+              value,
+              imagepath,
+            );
+            widget.onSave?.call(newItem, imageHasChanged);
+          },
+        ),
+      ],
+    );
   }
 
   Future<File?> _pickImage() async {
@@ -73,4 +169,3 @@ class _ItemChildEditorState extends State<ItemChildEditor> {
     return imageFile;
   }
 }
-
