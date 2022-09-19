587e80d00436ca8156741aaa291edba97e9b6a30
maheini35 <maheini35@me.com>
2022-09-19 23:07:35 +0200

    refactoring

--- lib/datamodels/itemchild.dart
+++ lib/datamodels/itemchild.dart
@@ -1,6 +1,11 @@
 class ItemChild {
   ItemChild(
-      this.id, this.parentID, this.description, this.value, this.imagepath);
+    this.id,
+    this.parentID,
+    this.description,
+    this.value,
+    this.imagepath,
+  );
   final int id;
   final int parentID;
   double value;
