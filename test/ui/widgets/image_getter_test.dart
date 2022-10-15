e2a290be22999ae2f516d14f685454ca30261bc0
maheini35 <maheini35@me.com>
2022-10-15 19:41:10 +0200

    started with imagegetter tests

---
+++ test/ui/widgets/image_getter_test.dart
@@ -0,0 +1,24 @@
+import 'package:carbpro/generated/l10n.dart';
+import 'package:carbpro/ui/widgets/image_getter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:mocktail/mocktail.dart';
+
+class mockImagePicker extends Mock implements ImagePicker {}
+
+void main() {
+  group(
+    'Test Layout',
+    () {
+    },
+  );
+
+    },
+  );
+
+  group('test functionality of the widget', () {
+  });
+}
